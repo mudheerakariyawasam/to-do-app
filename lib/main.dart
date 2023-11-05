00796fafecd71c99cb7706c665058b5e47a987de
Mudheera Kariyawasam <2020is055@stu.ucsc.cmb.ac.lk>
2023-11-05 23:31:07 +0530

    login and main pages connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/pages/auth_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:to_do_app/pages/taskadd_page.dart';
 import 'package:to_do_app/services/notification_service.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'firebase_options.dart';
@@ -23,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TaskManagementApp(),
-);
+      home: AuthPage(),
+    );
   }
 }
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:to_do_app/pages/home_page.dart';
+import 'package:to_do_app/pages/taskadd_page.dart';
 import 'package:to_do_app/pages/login_page.dart';
 
 class AuthPage extends StatelessWidget {
@@ -15,7 +15,7 @@ class AuthPage extends StatelessWidget {
             //user Logged In
 
             if (snapshot.hasData) {
-              return HomePage();
+              return TaskManagementApp();
             }
             //user not Logged In
             else {
